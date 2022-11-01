1df82c90b5891f00e28077abaefdba99655b6a3b
kos3l <kk@grumsendev.com>
2022-11-01 11:01:25 +0100

    kinda made tailwind work

--- app/src/view/Home.php
+++ app/src/view/Home.php
@@ -19,10 +19,14 @@ include_files(array(
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../../public/output.css" rel="stylesheet">
     <title>Home</title>
 </head>
 <body>
     <h1>Home here, only for router testing purposes for now</h1>
     <a href="/login"><button>go to login</button></a>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </body>
 </html>
