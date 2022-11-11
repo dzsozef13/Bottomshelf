9416f49853f2e92b4b7678faff8c3095af30bfe2
kos3l <kk@grumsendev.com>
2022-11-11 11:28:19 +0100

    moved all the class to input css

--- app/public/layouts/GuestLayout.php
+++ app/public/layouts/GuestLayout.php
@@ -9,10 +9,10 @@
     <title><?php echo $this->name ?></title>
 </head>
 
-<body class="bg-background-black-900 text-dim-white-900 font-sans">
-    <div class="mx-auto w-screen flex h-20 px-8  items-center fixed top-0 left-0 right-0">
+<body class="bodyStyle">
+    <div class="nav">
         <div class="w-full flex items-center">
-            <div class="w-8 h-8 bg-dim-white-900 rounded-full"></div>
+            <div class="logo"></div>
             <ul class="list-none flex items-center text-m uppercase">
                 <li class="mx-4 ml-6">
                     <a href="/Home">Home</a>
@@ -22,9 +22,9 @@
                 </li>
             </ul>
         </div>
-        <div class="w-40 h-8 flex justify-center items-center bg-highlight-green-900 rounded border-0 font-mono ">
+        <div class="btn">
             <a href="/Login">
-                <button class="uppercase text-background-black-900 font-bold text-sm">Sign in</button>
+                <button>Sign in</button>
             </a>
         </div>
     </div>
