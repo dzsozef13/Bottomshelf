c3394e52cab8d36a98fa7e164d22f7ae452522b9
kos3l <kk@grumsendev.com>
2022-11-13 16:23:43 +0100

    renamed front end controller to page controller

--- app/src/router/Routes.php
+++ app/src/router/Routes.php
@@ -8,11 +8,11 @@ include_files(array(
  */
 Router::add(new Route("404", "view/renderView", array("view" => "404")));
 
-Router::add(new Route("", "FrontEnd/homePage"));
-Router::add(new Route("Home", "FrontEnd/homePage"));
-Router::add(new Route("Login", "FrontEnd/loginPage"));
-Router::add(new Route("About", "FrontEnd/aboutPage"));
-Router::add(new Route("Dashboard", "view/renderView", array("view" => "dashboard")));
+Router::add(new Route("", "Page/homePage"));
+Router::add(new Route("Home", "Page/homePage"));
+Router::add(new Route("Login", "Page/loginPage"));
+Router::add(new Route("About", "Page/aboutPage"));
+Router::add(new Route("Dashboard", "Page/dashboardPage"));
 
 //test route but i was able to call a controller to create with this! really cool
 Router::add(new Route("CreatePost", "post/create"));
