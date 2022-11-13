5ee0c4b669f512658147aaa58d4b5b040d771f92
kos3l <kk@grumsendev.com>
2022-11-13 16:21:14 +0100

    added landing text to home

--- app/src/view/Home.php
+++ app/src/view/Home.php
@@ -1,1 +1,12 @@
-<p class="h-96">Home</p>
+<div class="grid grid-cols-6 gap-4">
+
+    <div class="col-span-2"></div>
+    <div class="col-span-2 px-1 h-80 flex items-end justify-center">
+        <h1 class="landing-headline ">Choose your drink for the night</h1>
+    </div>
+    <div class="col-span-2"></div>
+    <div class="col-span-6 flex items-start text-4xl justify-center">
+        <div class="landing-headline "> V</div>
+
+    </div>
+</div>
