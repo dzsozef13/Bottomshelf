a0a06f595e3f7a80715871f4431f8a8302b04ed2
kos3l <kk@grumsendev.com>
2022-11-13 21:28:13 +0100

    made login responsive

--- app/src/view/Login.php
+++ app/src/view/Login.php
@@ -1,7 +1,7 @@
 <div class="grid grid-cols-6 px-8 h-[calc(100vh-5rem)]">
     <div class="w-screen h-screen absolute top-0 left-0 z-0 bg-[radial-gradient(circle_at_100%_100%,_rgba(144,202,156,0.111)_0%,_rgba(144,202,156,0.0)_58%)]">
     </div>
-    <div class="col-span-2 flex flex-col justify-between pr-8 relative z-1">
+    <div class="col-span-6 sm:col-span-3 xl:col-span-2 flex flex-col justify-between sm:pr-8 relative z-1">
         <div class="mt-12">
             <div class="pb-6">
                 <h3 class="medium-headline mb-2">Sign in</h3>
@@ -31,5 +31,5 @@
 
         </div>
     </div>
-    <div class="col-span-4"></div>
+    <div class="col-span-6 sm:col-span-3 2xl:col-span-4 "></div>
 </div>
