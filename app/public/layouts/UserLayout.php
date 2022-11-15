922256ff848637ef87caec94d8bf2ddf0802c304
kos3l <kk@grumsendev.com>
2022-11-15 11:27:24 +0100

    added highlighting menu items if you are on the clicked page

--- app/public/layouts/UserLayout.php
+++ app/public/layouts/UserLayout.php
@@ -1,17 +1,29 @@
+<?php
+
+$test = new Router();
+?>
+
 <body class="bodyStyle">
     <div class="grid grid-cols-6 h-screen">
-        <div class="col-span-6 sm:col-span-3 xl:col-span-1 bg-highlight-green-900/10 p-8 flex flex-col justify-between">
+        <div class="sm:col-span-2 xl:col-span-1 bg-highlight-green-900/10 p-8 flex flex-col justify-between">
             <div>
                 <div class="logo"></div>
-                <ul>
+                <ul class="mt-6 uppercase">
                     <a href="/Dashboard">
-                        <li>Dashboard</li>
+                        <li class="mb-4 <?php if ($test->currentRoute === 'Dashboard') {
+                                            echo 'text-highlight-green-900';
+                                        } ?>">Dashboard</li>
                     </a>
                     <a href="/Explore">
-                        <li>Explore</li>
+                        <li class="mb-4 <?php if ($test->currentRoute === 'Explore') {
+                                            echo 'text-highlight-green-900';
+                                        } ?>">Explore</li>
                     </a>
                     <a href="/Profile">
-                        <li>My Profile</li>
+                        <li class="mb-4 <?php if ($test->currentRoute === 'Profile') {
+                                            echo 'text-highlight-green-900';
+                                        } ?>">
+                            My Profile</li>
                     </a>
                 </ul>
             </div>
@@ -21,7 +33,7 @@
             </div>
 
         </div>
-        <div class="col-span-6 sm:col-span-3 xl:col-span-5  ">
+        <div class=" sm:col-span-4 xl:col-span-5  ">
             {{content}}
         </div>
     </div>
