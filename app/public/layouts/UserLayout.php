da9f2ed96a895f063d78f1121fc57bc5490b0e71
kos3l <kk@grumsendev.com>
2022-11-26 15:01:56 +0100

    began setting up post display of a user

--- app/public/layouts/UserLayout.php
+++ app/public/layouts/UserLayout.php
@@ -1,7 +1,7 @@
 <!-- to be finished -->
 
 <body class="bodyStyle">
-    <div class="grid grid-cols-6 h-screen">
+    <div class="grid grid-cols-6 min-h-screen">
         <div class="col-span-1 bg-highlight-green-900/10 p-8 flex flex-col justify-between">
             <div>
                 <div class="logo"></div>
@@ -35,7 +35,7 @@
                 <h4 class="ml-4">{{username}}</h4>
             </div>
         </div>
-        <div class="col-span-5 bg-[linear-gradient(180deg,_rgba(26,30,29,1)_0%,_rgba(24,26,26,1)_10%,_rgba(21,22,23,1)_20%)]">
+        <div class="col-span-5 min-h-screen bg-[linear-gradient(180deg,_rgba(26,30,29,1)_0%,_rgba(24,26,26,1)_10%,_rgba(21,22,23,1)_20%)]">
             {{content}}
         </div>
     </div>
--- app/src/view/Profile.php
+++ app/src/view/Profile.php
@@ -1,13 +1,14 @@
 <?php
 $userCtrl = new UserController();
 $sessionsCtrl = new SessionController();
+$posts = new PostController();
 $user = $sessionsCtrl->getUser();
 $profile = null;
 if (isset($user)) {
     $profile = $userCtrl->fetchById($user['userId']);
 }
 ?>
-<div class="grid grid-cols-6 gap-4 px-8 w-full grid-flow-row ">
+<div class="grid grid-cols-6 gap-4 px-8 w-full grid-flow-row min-h-screen">
     <div class="col-span-6 2xl:h-[15vh] h-[25vh] "></div>
     <div class="2xl:col-span-1 col-span-0"></div>
     <div class="2xl:col-span-4 col-span-6 ">
@@ -40,5 +41,27 @@ if (isset($user)) {
 
         </div>
     </div>
-    <div class="2xl:col-span-1 col-span-0"></div>
+    <div class="2xl:col-span-1 col-span-0"> </div>
+
+    <div class="col-span-6 profile-post-options-container">
+        <a href="">
+            <div class="option-chip">
+                Public
+            </div>
+        </a>
+        <a href="">
+            <div class="option-chip">
+                Private
+            </div>
+        </a>
+    </div>
+    <div class="col-span-6">
+        <div class="grid grid-cols-4 gap-4 ">
+            <div class="bg-background-secondary-900 h-64">1</div>
+            <div class="bg-background-secondary-900 h-64">2</div>
+            <div class="bg-background-secondary-900 h-64">3</div>
+            <div class="bg-background-secondary-900 h-64">4</div>
+            <div class="bg-background-secondary-900 h-64">5</div>
+        </div>
+    </div>
 </div>
