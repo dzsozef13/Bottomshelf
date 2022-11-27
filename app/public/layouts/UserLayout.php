870d14652094f5e1d5e21011ce803d2c41f16d1f
kos3l <kk@grumsendev.com>
2022-11-27 15:15:32 +0100

    for now resolved the issue of overflowing username in userlayout

--- app/public/layouts/UserLayout.php
+++ app/public/layouts/UserLayout.php
@@ -2,8 +2,8 @@
 
 <body class="bodyStyle">
     <div class="grid grid-cols-6 min-h-screen">
-        <div class="col-span-1 bg-highlight-green-900/10 p-8 flex flex-col justify-between">
-            <div>
+        <div class="col-span-2 lg:col-span-1 bg-highlight-green-900/10 p-8 flex flex-col justify-between">
+            <div class="h-auto w-full">
                 <div class="logo"></div>
                 <ul class="mt-6 uppercase">
                     <a href="/Dashboard">
@@ -32,14 +32,16 @@
                     </a>
                 </ul>
             </div>
-            <div class="w-full flex items-center relative">
-                <div class="logo flex justify-center items-center">
+            <div class="w-full flex items-center relative break-all h-auto ">
+                <div class="logo flex-none flex justify-center items-center">
                     <i class="las la-smile text-background-black-900 text-3xl"></i>
                 </div>
-                <h4 class="ml-4">{{username}}</h4>
+                <a href="/Profile" class="w-full h-auto">
+                    <h4 class="ml-4 ">{{username}}</h4>
+                </a>
             </div>
         </div>
-        <div class="col-span-5 min-h-screen bg-[linear-gradient(180deg,_rgba(26,30,29,1)_0%,_rgba(24,26,26,1)_10%,_rgba(21,22,23,1)_20%)]">
+        <div class="col-span-4 lg:col-span-5 min-h-screen bg-[linear-gradient(180deg,_rgba(26,30,29,1)_0%,_rgba(24,26,26,1)_10%,_rgba(21,22,23,1)_20%)]">
             {{content}}
         </div>
     </div>
