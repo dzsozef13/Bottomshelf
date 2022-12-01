12a10c25abea0953d109ce0ef072f6c84c3a80bb
kos3l <kk@grumsendev.com>
2022-12-01 13:20:47 +0100

    fixed merge issue

--- app/src/view/auth/Explore.php
+++ app/src/view/auth/Explore.php
@@ -16,10 +16,11 @@ $posts = $postController->fetchAll();
  * Media controller
  */
 $mediaController = new MediaController();
+
 ?>
 
 <!-- Explore View -->
-<div class="grid grid-cols-3 gap-8 px-8 h-[calc(100vh-5rem)]">
+<div class="grid grid-cols-6 px-8 h-[calc(100vh-5rem)]">
     <?php
     /**
      * Loop through the posts
@@ -30,21 +31,19 @@ $mediaController = new MediaController();
         // This will require some guarding, specially if we're planning to enable posting without images
         $media = $mediaController->fetchMediaForPost($post->getId());
         $indexedMediaArray = array_values($media);
+        $coverImageBlob = $indexedMediaArray[0]->getImage();
 
-        $coverImageBlob =  $indexedMediaArray[0]->getImage();
         // Fill the template with data
         $postCard = str_replace(
             array(
                 '{{title}}',
-                '{{username}}',
-                '{{imageBlob}}',
-                '{{latestComment}}'
+                '{{description}}',
+                '{{imageBlob}}'
             ),
             array(
                 $post->getTitle(),
-                $post->getAuthorName(),
-                base64_encode($coverImageBlob),
-                $post->getLatestComment(),
+                $post->getDescription(),
+                base64_encode($coverImageBlob)
             ),
             $postCardTempalte
         );
@@ -56,7 +55,7 @@ $mediaController = new MediaController();
 </div>
 
 <!-- Test File Upload Form -->
-<form action="MediaUpload" method="post">
+<form action="MediaUpload" method="post" enctype="multipart/form-data">
     <div class="input-field-wrapper">
         <div class="icon-wrapper">
             <i class="las la-at"></i>
@@ -65,5 +64,5 @@ $mediaController = new MediaController();
         <input placeholder="Image" class="input-field " type="file" name="media2"><br>
         <input placeholder="Image" class="input-field " type="file" name="media3"><br>
     </div>
-    <button class="btn-white w-full mt-6" type="submit">UPLOAD</button>
+    <button class="btn-white w-full mt-6" type="submit" name="submit">UPLOAD</button>
 </form>
