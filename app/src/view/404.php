fe3cb01c9d2e67a9021b2e3f688e4bd39440f135
kos3l <kk@grumsendev.com>
2022-12-14 17:39:52 +0100

    updated 404 page

--- app/src/view/404.php
+++ app/src/view/404.php
@@ -1,2 +1,4 @@
-<h1>404 - Page not found</h1>
-<a href="Home"> Back to home </a>
+<div class="w-full h-[calc(100vh-5rem)] flex justify-center items-center flex-col">
+    <h1 class="text-2xl mb-4">404 - Page not found</h1>
+    <a href="Home"> <button class="btn btn-green">Back to home </button> </a>
+</div>
